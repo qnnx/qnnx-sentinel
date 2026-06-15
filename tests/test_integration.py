fddeab71017c368b939609d856f16bd2ae7f73c1
Somil36 <narangsomil@gmail.com>
2026-06-15 13:28:53 +0530

    tested

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -26,8 +26,8 @@ API_PREFIX = "/api/v1"
 
 # Hardcode your credentials from Supabase
 test_state = {
-    "api_key_id": "184acd71-c4ff-420c-bd16-c19148ffd6db", # The UUID from SQL
-    "api_key": "qnnx_somiltestkey123",
+    "api_key_id": "API_KEY_ID", # The UUID from SQL
+    "api_key": "API_KEY",
     "signing_secret": "ignored",
     "public_key": None # Will be populated by the keygen test
 }
